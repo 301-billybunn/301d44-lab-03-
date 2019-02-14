6ddcce554e4b6da1c0f58107edf87c041ff29f4a
IanGifford261 <lightyagamie10@gmail.com>
2019-02-14 10:30:08 -0800

    Creates the Render function, and the readJson

--- js/app.js
+++ js/app.js
@@ -4,8 +4,33 @@ function Animal(animal) {
   this.keyword = animal.keyword;
   this.horns = animal.horns;
   this.title = animal.title;
-  this.discription = animal.description;
+  this.description = animal.description;
   this.image_url = animal.image_url;
 }
 
 Animal.allAnimals = [];
+
+Animal.prototype.render = function() {
+  $('main').append('<div class="clone"></div>');
+  let animalClone = $('div[class="clone"]');
+
+  let animalHtml = $('#photo-template').html();
+
+  animalClone.html(animalHtml);
+  animalClone.find('h2').text(this.keyword);
+  animalClone.find('img').attr('src', this.image_url);
+  animalClone.find('p').text(this.description);
+  animalClone.removeClass('clone');
+  animalClone.attr('class', this.keyword);
+}
+
+Animal.readJson = () => {
+  $.get('data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(obj =>{
+        Animal.allAnimals.push(new Animal(obj));
+      })
+    })
+    .then(Animal.loadAnimals)
+};
+
