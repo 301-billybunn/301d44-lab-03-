81d010235a9946ef5c84b2383c5de986a4d85ea1
Billy Bunn <williammbunn@gmail.com>
2019-03-03 15:38:42 -0800

    Refactors app.js. Works with one JSON data file at a time

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 'use strict';
 
+Animal.allAnimals = []; // Array to hold animal objects
+
+// Constructor for each animal tile
 function Animal(animal) {
   this.keyword = animal.keyword;
   this.horns = animal.horns;
   this.title = animal.title;
   this.description = animal.description;
   this.image_url = animal.image_url;
+  Animal.allAnimals.push(this); // each instance gets pushed to the allAnimals array
 }
 
-Animal.allAnimals = [];
-
+// Prototype function to create DOM variables for each animal
 Animal.prototype.render = function () {
   $('main').append('<div class="clone"></div>');
   let animalClone = $('div[class="clone"]');
@@ -24,8 +27,9 @@ Animal.prototype.render = function () {
   animalClone.attr('class', this.keyword);
 }
 
+// Renders change in select box
 Animal.prototype.renderSelChange = function () {
-  console.log('ranrenderSelChange');
+  // console.log('ranrenderSelChange');
   $('select').append('<option class="clone"></option>');
   let optClone = $('option[class="clone"]');
 
@@ -34,21 +38,15 @@ Animal.prototype.renderSelChange = function () {
   optClone.removeClass('clone');
 }
 
-const jsonDataOne = 'data/page-1.json'
-const jsonDataTwo = 'data/page-2.json';
-
+// Function to take JSON data and run each obj through the constructor
 Animal.readJson = (jsonPage) => {
   $.get(jsonPage)
-    .then(data => {
-      data.forEach(obj => {
-        Animal.allAnimals.push(new Animal(obj));
-      })
-    })
+    .then(data => data.forEach(obj => new Animal(obj)))
     .then(Animal.loadAnimals)
 };
 
 Animal.loadAnimals = () => {
-  console.log('loaded animals');
+  console.log('fired loadAnimals()');
   Animal.allAnimals.forEach(animal => animal.render())
   Animal.allAnimals.forEach(animal => animal.renderSelChange())
 }
@@ -59,6 +57,10 @@ $('select[name="animals"]').on('change', function () {
   $('div').hide()
   $(`div[class="${$selection}"]`).show()
 })
+
+function switchEventHandler() {
+  console.log('fired switchEventHandler()');
+}
 
 function switchEventHandler() {
   console.log('heard click');
@@ -75,4 +77,9 @@ function switchEventHandler() {
 // SWITCH WHICH GALLERY IS DISPLAYS ON PAGE
 $('.switch-button').on('click', switchEventHandler)
 
+
+
+const jsonDataOne = 'data/page-1.json'
+// const jsonDataTwo = 'data/page-2.json';
 $(() => Animal.readJson(jsonDataOne));
+// $(() => Animal.readJson(jsonDataTwo));
